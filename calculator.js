e64c1fd421fdfadf5c6c3961cddaa0ad9445813c
Roland Manangan <roland.manangan@gmail.com>
2018-12-13 21:19:22 -1000

    step one took 80% of the time

--- calculator.js
+++ calculator.js
@@ -5,61 +5,60 @@
  * @variable PRIVATE { Number } `total`
  * @return {object} `calculator` object that can be used
  */
+var calculatorModule = function(){
+    var memory = 0;
+    var total = 0;
+    function validate(x){
+        if(typeof x !== 'number'){
+            throw error;
+        }
+    }
+    var calculator = {
 
+        load: function(number){
+            validate(number);
+            total = number;
+            return total;
+        },
 
-  /**
-   * sets the `total` to the number passed in
-   * @param  { Number } x
-   * @return { Number }    current total
-   */
-
-
-  /**
-   * Return the value of `total`
-   * @return { Number }
-   */
-
-
-  /**
-   * Sums the value passed in with `total`
-   * @param { Number } x
-   */
-
-
-  /**
-   * Subtracts the value passed in from `total`
-   * @param  { Number } x
-   */
-
-
-  /**
-   * Multiplies the value by `total`
-   * @param  { Number } x
-   */
-
-
-  /**
-   * Divides the value passing in by `total`
-   * @param  { Number } x
-   */
-
-
-  /**
-   * Return the value stored at `memory`
-   * @return { Number }
-   */
-
-
-  /**
-   * Stores the value of `total` to `memory`
-   */
-
-
-  /**
-   * Clear the value stored at `memory`
-   */
+        getTotal: function(){
+            return total;
+        },
+    
+        add: function(number){
+            validate(number);
+            total += number;
+            return total;
+        },
+        subtract: function(number){
+            validate(number);
+            total -= number;
+            return total;
+        },
+        multiply: function(number){
+            validate(number);
+            total *= number;
+            return total;
+        },
+        divide: function(number){
+            validate(number);
+            total /= number;
+            return total;
+        },
+        recallMemory: function(){
+            return memory;
+        },
+        saveMemory: function(){
+            memory = total;
+        },
+        clearMemory: function(){
+            memory = 0;
+        }
 
   /**
    * Validation
    */
 
+};
+  return calculator;
+}
